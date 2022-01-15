058f67c49841359651c7df5910329625c64aa437
Thomas McGall <thomasm07@ucla.edu>
2022-01-15 15:40:16 -0800

    second commit of public info

--- src/pages/projects-page.js
+++ src/pages/projects-page.js
@@ -12,13 +12,13 @@ const projectsPage = () => (
     
     
     <h1>BrailleBoard</h1>
-    <p>BrailelBoard is a teaching tool for the visually impaired. 
-    The traditional Qwerty Keyboard is unintuitive and maybe be 
+    <p>BrailleBoard is a teaching tool for the visually impaired as well as those seeking to learn Braille. 
+    The traditional QWERTY keyboard is unintuitive and maybe be 
     difficult to use for young visually impaired users.
-    BrailleBoard utilizes the T,Y,G,H,B,N keys to simulate
-    the six dots of the braille system. When using Braille Board
-    seperate each "Braile letter" with a space. Two consecutive spaces
-    will be the equivalent of a translated space. When your sentence is finished press enter. </p>
+    BrailleBoard utilizes the T,Y,G,H,B, and N keys to simulate
+    the six dots of the Braille system. For example, "yghb " is equivalent to the letter T. When using BrailleBoard,
+    seperate each "Braille letter" with a space. Two consecutive spaces
+    will be the equivalent of a translated space. When your sentence is finished press enter. <strong>For example, try typing "yghn th tgb ty thb ytb th " below.</strong> </p>
 
     <StaticImage src="../images/braille.png" alt="A dinosaur" /> 
     
